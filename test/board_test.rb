ad9a2d65e1c16719565b18cc4cc7e255b7088f13
Ryan Hantak <rhantak@gmail.com>
2019-08-20 16:59:39 -0600

    Add test to board_test to validate ship placement
    Co-authored-by: Jonathan Patterson <47950816+jonpatt92@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -14,7 +14,25 @@ class BoardTest < Minitest::Test
   end
 
   def test_can_validate_a_coordinate
-    assert_equal true, valid_coordinate?("A1")
-    assert_equal false, valid_coordinate?("E14")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal false, @board.valid_coordinate?("E14")
+  end
+
+  def test_can_validate_ship_placement
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1","B1","C1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "B1"])
+
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@cruiser,["A1", "B2"])
+    assert_equal false, @board.valid_placement?(@submarine,["A1", "A3"])
+    assert_equal false, @board.valid_placement?(@submarine,["A1", "B2"])
+    assert_equal false, @board.valid_placement?(@submarine,["A1", "B2", "C3"])
+
   end
 end
