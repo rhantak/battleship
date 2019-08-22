8311ffafef6f6f617b8e52e78a96b6f157328c01
Jon Patterson <JonPatt92@gmail.com>
2019-08-22 13:55:22 -0600

    Add test for verifying ship not already placed to board_test
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -15,10 +15,10 @@ class BoardTest < Minitest::Test
 
   def test_can_validate_a_coordinate
     assert_equal true, @board.valid_coordinate?("A1")
-    assert_equal false, @board.valid_coordinate?("E14")
+    refute @board.valid_coordinate?("E14")
   end
 
-  def test_can_validate_ship_placement
+  def test_can_validate_ship_placement_is_valid
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
 
@@ -27,12 +27,23 @@ class BoardTest < Minitest::Test
     assert_equal true, @board.valid_placement?(@cruiser, ["A1","B1","C1"])
     assert_equal true, @board.valid_placement?(@submarine, ["A1", "B1"])
 
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
-    assert_equal false, @board.valid_placement?(@cruiser,["A1", "B2"])
-    assert_equal false, @board.valid_placement?(@submarine,["A1", "A3"])
-    assert_equal false, @board.valid_placement?(@submarine,["A1", "B2"])
-    assert_equal false, @board.valid_placement?(@submarine,["A1", "B2", "C3"])
+    refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@cruiser,["A1", "B2"])
+    refute @board.valid_placement?(@submarine,["A1", "A3"])
+    refute @board.valid_placement?(@submarine,["A1", "B2"])
+    refute @board.valid_placement?(@submarine,["A1", "B2", "C3"])
+  end
 
+  def test_can_validate_ship_placement_doesnt_overlap
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @board.cells["A3"].place_ship(@cruiser)
+    @board.cells["B3"].place_ship(@cruiser) 
+    @board.cells["C3"].place_ship(@cruiser)
+
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+
+    refute @board.valid_placement?(@submarine, ["A2", "A3"])
   end
 end
