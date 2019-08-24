ef1078648c654eb359bff79d4e192cc9361bf083
Jon Patterson <JonPatt92@gmail.com>
2019-08-24 17:30:46 -0600

    Add formatting, end game. Fully functioning iteration 3
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -64,10 +64,10 @@ class Board
   end
 
   def render_board(show = false)
-    "\s \s \s \s 1   2   3   4\n\
-    A   #{@cells["A1"].render(show)}   #{@cells["A2"].render(show)}   #{@cells["A3"].render(show)}   #{@cells["A4"].render(show)}\n\
-    B   #{@cells["B1"].render(show)}   #{@cells["B2"].render(show)}   #{@cells["B3"].render(show)}   #{@cells["B4"].render(show)}\n\
-    C   #{@cells["C1"].render(show)}   #{@cells["C2"].render(show)}   #{@cells["C3"].render(show)}   #{@cells["C4"].render(show)}\n\
-    D   #{@cells["D1"].render(show)}   #{@cells["D2"].render(show)}   #{@cells["D3"].render(show)}   #{@cells["D4"].render(show)}\n"
+    "\s \s \s  1   2   3   4\n\
+    A  #{@cells["A1"].render(show)}   #{@cells["A2"].render(show)}   #{@cells["A3"].render(show)}   #{@cells["A4"].render(show)}\n\
+    B  #{@cells["B1"].render(show)}   #{@cells["B2"].render(show)}   #{@cells["B3"].render(show)}   #{@cells["B4"].render(show)}\n\
+    C  #{@cells["C1"].render(show)}   #{@cells["C2"].render(show)}   #{@cells["C3"].render(show)}   #{@cells["C4"].render(show)}\n\
+    D  #{@cells["D1"].render(show)}   #{@cells["D2"].render(show)}   #{@cells["D3"].render(show)}   #{@cells["D4"].render(show)}\n"
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require 'pry'
+
 class Game
   attr_reader :player_board, :computer_board
 
@@ -13,17 +14,19 @@ class Game
   def start_game
     quit = nil
     until quit == "q"
-      p "Welcome to BATTLESHIP"
+      p "Welcome to BATTLESHIP Comrade!"
       p "Enter 'p' to play. Enter 'q' to quit."
       valid_input = false
       until valid_input
         initial_input = gets.chomp
         if initial_input.downcase == "p"
           valid_input = true
+          initialize()
           play_game()
         elsif initial_input.downcase == "q"
           valid_input = true
           quit = "q"
+          return "Thanks for playing!"
         else p "That was not a valid input. Enter 'p' to play. Enter 'q' to quit."
         end
       end
@@ -34,7 +37,26 @@ class Game
     computer_place_cruiser
     computer_place_sub
     player_place_ships
-    10.times do take_turn
+    computer_loss = false
+    player_loss = false
+    until computer_loss || player_loss
+      take_turn
+      if @cruiser.sunk? && @computer_cruiser.sunk? && @sub.sunk? && @computer_sub.sunk?
+        puts "You and your enemy sunk each other's last ship at the same time!"
+        puts "Mutually assured destruction!"
+      elsif @cruiser.sunk? && @sub.sunk?
+        player_loss = true
+        puts "The enemy sunk all your ships! A loss for the Motherland!"
+        puts " "
+        puts "=" * 40
+        puts " "
+      elsif @computer_cruiser.sunk? && @computer_sub.sunk?
+        computer_loss = true
+        puts "You sunk all your enemy's ships! A victory for the Motherland!"
+        puts " "
+        puts "=" * 40
+        puts " "
+      end
     end
   end
 
@@ -42,10 +64,13 @@ class Game
     @cruiser = Ship.new("Cruiser", 3)
     @sub = Ship.new("Submarine", 2)
 
-    puts "Your opponent has laid out their ships on the grid."
+    system "clear"
+    puts "Your enemy has laid out their ships on the grid."
     puts "You now need to place your two ships."
     puts "The Cruiser is 3 units long and the Submarine is 2 units long."
+    puts " "
     print @player_board.render_board(true)
+    puts " "
     puts "Enter the squares for your Cruiser (3 spaces) separated by spaces"
     puts "Example: A1 A2 A3"
 
@@ -54,14 +79,15 @@ class Game
       print "> "
       cruiser_spaces = gets.chomp.upcase
       cruiser_array = cruiser_spaces.split(" ").to_a
-      if @player_board.valid_placement?(@cruiser, cruiser_array)
+      if @player_board.valid_placement?(@cruiser, cruiser_array.sort)
         good_cruiser = true
-        @player_board.place(@cruiser, cruiser_array)
+        @player_board.place(@cruiser, cruiser_array.sort)
       else
-        puts "That's an invalid placement shipmate, please try again."
+        puts "That's an invalid placement comrade, please try again."
       end
     end
 
+    system "clear"
     print @player_board.render_board(true)
     puts "Enter the squares for your Submarine (2 spaces)"
 
@@ -70,13 +96,14 @@ class Game
       print "> "
       sub_spaces = gets.chomp.upcase
       sub_array = sub_spaces.split(" ").to_a
-      if @player_board.valid_placement?(@sub, sub_array)
+      if @player_board.valid_placement?(@sub, sub_array.sort)
         good_sub = true
-        @player_board.place(@sub, sub_array)
+        @player_board.place(@sub, sub_array.sort)
       else
-        puts "That's an invalid placement shipmate, please try again."
+        puts "That's an invalid placement comrade, please try again."
       end
     end
+    system "clear"
   end
 
   def computer_place_cruiser
@@ -100,11 +127,13 @@ class Game
   def take_turn
     puts "=============COMPUTER BOARD============="
     print @computer_board.render_board
+    puts " "
     puts "==============PLAYER BOARD=============="
     print @player_board.render_board(true)
+    puts " "
     puts "============Battle Stations!============"
 
-    puts "Captain, enemy ships detected. Where should we fire?"
+    puts "Comrade, enemy ships detected. Where should we fire?"
     print "Enter your firing coordinate: "
     good_shot = false
     until good_shot
@@ -114,19 +143,22 @@ class Game
          @computer_board.cells[player_shot].fire_upon
          good_shot = true
       else
-         puts "I don't think that's wise Captain."
+         puts "I don't think that's wise Comrade."
          puts "Please enter a valid firing coordinate you haven't already chosen."
       end
     end
+
     shot_result = nil
+
     if @computer_board.cells[player_shot].render == "M"
       shot_result = "was a miss"
     elsif @computer_board.cells[player_shot].render == "H"
       shot_result = "was a hit"
     elsif @computer_board.cells[player_shot].render == "X"
-      shot_result = "sunk the opponent's #{@computer_board.cells[player_shot].ship.name}"
+      shot_result = "sunk the enemy's #{@computer_board.cells[player_shot].ship.name}"
     end
-    puts "Your shot at #{player_shot} #{shot_result}!"
+    system "clear"
+    puts "Comrade, your shot at #{player_shot} #{shot_result}!"
     computer_take_turn
   end
 
@@ -148,12 +180,11 @@ class Game
     elsif @player_board.cells[computer_shot[0]].render == "X"
       shot_result = "sunk your #{@player_board.cells[computer_shot[0]].ship.name}"
     end
-    puts "Your opponent's shot at #{computer_shot[0]} #{shot_result}!"
+    puts "The enemy's shot at #{computer_shot[0]} #{shot_result}!"
+    puts "========================================"
+    puts " "
   end
 end
-
-
-
 
 @game = Game.new
 binding.pry
